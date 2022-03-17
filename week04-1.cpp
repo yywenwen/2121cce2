30a569378c131c51c9525825d8d23c7349d1349f
yywenwen <100347086+yywenwen@users.noreply.github.com>
2022-03-17 16:13:15 +0800

    Add files via upload

---
+++ week04-1.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+char line[2000];
+int main()
+{
+	int t=1;
+	while(gets(line))
+	{
+		int ans[256]={};
+		if(t > 1)
+			printf("\n");
+		for(int i=0; line[i]!=0; i++)
+		{
+			char c = line[i];
+			ans[c]++;
+		}
+		for(int c=128; c>31; c--)
+		{
+			if(ans[c] != 0)
+				printf("%d %d\n", c, ans[c]);
+		}
+		t++;
+	}
+}
